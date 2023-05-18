15bade8209e2f2d07abd6de91d9faa77a872849e
akshpatel24 <133981608+akshpatel24@users.noreply.github.com>
2023-05-18 09:48:47 -0400

    Add files via upload

---
+++ instance example.py
@@ -0,0 +1,12 @@
+#instance bound to object of class. Used to acess all atributes and all conjucntion.
+class Student:
+    # constructor
+    def __init__(self, name, age):
+        # Instance variable
+        self.name = name
+        self.age = age
+
+# create first object
+s1 = Student("Aksh",20)
+s2=Student("Hello",35)
+
